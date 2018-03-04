fd3a488ac8b90f9561800e4f9bde24208260c89f
goodship1 <msgoodship1@gmail.com>
2018-03-04 23:46:10 +0000

    adding tests and function declarations

--- PyVerge/pyVerge.py
+++ PyVerge/pyVerge.py
@@ -1,4 +1,18 @@
 import requests
 
 class PyVerge(object):
-	pass
+	
+	def get_Blockcount(self):
+		pass
+	
+	def get_Blockindex(self,index):
+		pass
+	
+	def get_Block(self):
+		pass
+	
+	def get_Top(self,top_accounts):
+		pass
+	
+
+	
---
+++ Tests/Tests.py
@@ -0,0 +1,14 @@
+import pytest
+import requests
+
+def test_Blockcount():
+	pass
+	
+def test_getBlockindex():
+	pass
+	
+def test_getBlock():
+	pass
+
+def get_top():
+	pass
