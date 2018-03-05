6a6e2a66a8b10ce06937cbf26200d77b174a477e
goodship1 <msgoodship1@gmail.com>
2018-03-05 22:38:23 +0000

    adding get block connections

--- PyVerge/pyVerge.py
+++ PyVerge/pyVerge.py
@@ -1,10 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
+import urllib3
+urllib3.disable_warnings()
+
 
 class PyVerge(object):
 	
 	def get_Blockcount(self):
-		pass
+		url = "https://verge-blockchain.info/api/getconnectioncount"
+		request_To_verge = requests.get(url,verify = False)
+		return request_To_verge.text
+		
 	
 	def get_Blockindex(self,index):
 		pass
@@ -22,4 +27,5 @@ class PyVerge(object):
 		pass
 	
 
-	
+c = PyVerge()
+print(c.get_Blockcount())
