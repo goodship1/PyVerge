1a31c837913dacb0df744fae8a2a0a7fb60f4e43
goodship1 <msgoodship1@gmail.com>
2018-03-07 21:55:39 +0000

    started to implemented tests

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,23 +1,33 @@
 import pytest
 import requests
+import sys
+sys.path.insert(0,'../PyVerge/')
+from pyVerge import PyVerge
+
+verge  = PyVerge()
 
 def test_Blockcount():
-	pass
-	
+	url = "https://verge-blockchain.info/api/getblockcount"
+	request_To_verge = requests.get(url,verify = False)
+	assert verge.get_Blockcount() == request_To_verge.text
 
 def test_Get_connectioncount():
-	pass
+	url = "https://verge-blockchain.info/api/getconnectioncount"
+	request_To_verge = requests.get(url,verify =  False)
+	assert verge.get_Connectioncount() ==  request_To_verge.text
+	
 	
 def test_Get_blockhash():
-	pass
-	
-def test_Get_Block():
-	pass
+	index = 0#gensis block location
+	url = "https://verge-blockchain.info/api/getblockhash?index=%s"%index
+	request_To_verge = requests.get(url,verify = False)
+	assert verge.get_Blockhash(index) == request_To_verge.text
+
 
 def test_Get_top():
 	pass
 
-def test_Get_moneysupply(self):
+def test_Get_moneysupply():
 	pass
 
 def test_Get_wealthdistro():
@@ -27,7 +37,7 @@ def test_Get_wealthdistro():
 def test_Get_satoshi():
 	pass
 
-def test_Get_Movement():
+def test_Get_movement():
 	pass
 	
 
