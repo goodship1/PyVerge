7ad4f4c624d012e09ddccb9579147b951000f1c0
goodship1 <msgoodship1@gmail.com>
2018-03-10 23:23:34 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	  name = 'pyVerge',
+	  version = '1.0',
+	  description ='libary to interact with verge blockchain',
+	  author ='gavin goodship',
+	  packages=['pyVerge'],
+	  install_requires=["requests"]
+	  )
